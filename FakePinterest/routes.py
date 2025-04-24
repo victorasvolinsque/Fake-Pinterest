72d9c40e93be12c05c1d1278cf96e0a06fa032eb
Victor Asvolinsque <victor.asvolinsque@icloud.com>
2025-04-24 16:45:43 -0300

    Implementando Login e Segurança

--- FakePinterest/routes.py
+++ FakePinterest/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template
 from flask import url_for
 from fakepinterest import app
+from flask_login import login_required
 
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
 
 @app.route('/perfil/<usuario>')
+@login_required
 def perfil(usuario):
     return render_template('perfil.html', usuario=usuario) 
